c9db83850522bfecb86af483dafa81c01579ef4e
sofia serrano <sofiaserrano498@gmail.com>
2026-06-18 01:16:37 -0300

    se agrega  referencia a Jasmine

--- apps/mobile/src/app/app.component.spec.ts
+++ apps/mobile/src/app/app.component.spec.ts
@@ -1,3 +1,5 @@
+/// <reference types="jasmine" />
+
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 
--- apps/mobile/src/app/login/login.page.spec.ts
+++ apps/mobile/src/app/login/login.page.spec.ts
@@ -1,3 +1,5 @@
+/// <reference types="jasmine" />
+
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
